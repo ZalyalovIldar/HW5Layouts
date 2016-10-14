70af6610f5ce184b888992d918bc6b9eb0257bea
Ильдар Залялов <Zalyalov.Ild@gmail.com>
2016-10-14 23:12:35 +0300

    Initial Commit

---
+++ HWLayouts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HWLayouts
+//
+//  Created by Ildar Zalyalov on 14.10.16.
+//  Copyright © 2016 com.flatstack.firebase. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
